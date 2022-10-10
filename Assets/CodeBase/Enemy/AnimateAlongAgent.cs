0a47535ca3214b909e7fa1c13e237c1b057972a4
Anton Pashkevich <natoy7000@gmail.com>
2022-10-10 15:42:19 +0200

    Animator enemy

---
+++ Assets/CodeBase/Enemy/AnimateAlongAgent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace CodeBase.Enemy
+{
+    public class AnimateAlongAgent : MonoBehaviour
+    {
+
+        [SerializeField] private NavMeshAgent _agent;
+        [SerializeField] private EnemyAnimator _animator;
+        [SerializeField] private float _minimalVelocity = 0.1f;
+        
+        private void Update() {
+            if(ShouldMove())
+                _animator.Move(_agent.velocity.magnitude);
+            else
+                _animator.StopMoving();
+        }
+
+        private bool ShouldMove() => 
+            _agent.velocity.magnitude > _minimalVelocity && _agent.remainingDistance > _agent.radius;
+    }
+}
