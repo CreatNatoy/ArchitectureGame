22d7bd84c11e5f2368687dca0ebb95dd0930fd4e
Anton Pashkevich <natoy7000@gmail.com>
2022-10-14 13:24:50 +0200

    Enemy Death

---
+++ Assets/CodeBase/Enemy/EnemyDeath.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    public class EnemyDeath : MonoBehaviour
+    {
+        [SerializeField] private EnemyHealth _health;
+        [SerializeField] private EnemyAnimator _animator;
+        [SerializeField] private GameObject _deathFX;
+
+        public event Action Happened;
+
+        private void Start() => 
+            _health.HealthChanged += HealthChanged;
+
+        private void OnDestroy() => 
+            _health.HealthChanged -= HealthChanged;
+
+        private void HealthChanged() {
+            if (_health.Current <= 0)
+                Die();
+        }
+
+        private void Die() {
+            _health.HealthChanged -= HealthChanged;
+            
+            _animator.PlayDeath();
+
+            SpawnDeathFx();
+            StartCoroutine(DestroyTimer());
+            
+            Happened?.Invoke();
+        }
+
+        private void SpawnDeathFx() => 
+            Instantiate(_deathFX, transform.position, Quaternion.identity);
+
+        private IEnumerator DestroyTimer() {
+            yield return new WaitForSeconds(3); 
+            Destroy(gameObject);
+        }
+    }
+}
